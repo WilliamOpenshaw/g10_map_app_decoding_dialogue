03a64cf7767f109d6877444808b32af3cd8a29cc
OphaTapioka <ophashias@gmail.com>
2024-09-19 13:43:00 +0800

    0.0.7
    Added comments to our hoverswell script

--- Assets/Scripts/hoverSwell.cs
+++ Assets/Scripts/hoverSwell.cs
@@ -1,30 +1,48 @@
 using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;// Required when using Event data.
+using UnityEngine.UI; // Required to change UI elements in Unity
+using UnityEngine.EventSystems; // Required when using Event data.
 
-public class hoverSwell : MonoBehaviour, IPointerEnterHandler,  IPointerExitHandler// required interface when using the OnPointerEnter method.
-{
-    public float sX;
-    public float sY;
-    public float sZ;
+
+//This is our script class
+public class hoverSwell : MonoBehaviour, IPointerEnterHandler,  IPointerExitHandler //required for mouse over effects
+{   
+    //create variable for sizeX
+    public float sizeX;
+    //create variable for sizeX
+    public float sizeY;
+    //create variable for sizeX
+    public float sizeZ;
+
+    //Do this once when program starts
     public void Start()
-    {
-        sX = gameObject.transform.localScale.x;
-        sY = gameObject.transform.localScale.y;
-        sZ = gameObject.transform.localScale.z;
+    {   
+        //save starting x size
+        sizeX = gameObject.transform.localScale.x;
+        //save starting y size
+        sizeY = gameObject.transform.localScale.y;
+        //save starting z size
+        sizeZ = gameObject.transform.localScale.z;
     }
-    //Do this when the cursor enters the rect area of this selectable UI object.
+
+    //Do this when the cursor exits the rect area of this selectable UI object.
     public void OnPointerExit(PointerEventData eventData2)
     {
+        //send a message to console if exit
         Debug.Log("The cursor EXITED the selectable UI element.");
-        gameObject.transform.localScale = new Vector3(  sX, sY, sZ );
+
+        //double the size of this object that the script is attached to
+        gameObject.transform.localScale = new Vector3(  sizeX, sizeY, sizeZ );
     }
-    
+
+    //Do this when the cursor enters the rect area of this selectable UI object.
     public void OnPointerEnter(PointerEventData eventData)
     {
+        //send a message to console if enter
         Debug.Log("The cursor entered the selectable UI element.");
-        gameObject.transform.localScale = new Vector3(  sX * 2, sY * 2 , sZ * 2 );
+        
+        //double the size of this object that the script is attached to
+        gameObject.transform.localScale = new Vector3(  sizeX * 2, sizeY * 2 , sizeZ * 2 );
     }
 
     
