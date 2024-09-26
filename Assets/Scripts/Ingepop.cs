0ba5df16b9fef2155b4e19c3d277db09ee53992c
Inge-S <inge.smidt@vis.tp.edu.tw>
2024-09-26 05:55:20 +0000

    Updates
    I updated most of EU to pop by 24 times

--- Assets/Scripts/Ingepop.cs
+++ Assets/Scripts/Ingepop.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
-using System.Collections
-using UnityEngine.UI
-using UnityEngine.EventSystems
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class Ingepop : MonoBehaviour, IPointerEnterHandler, IPointerExitHanler
+public class Ingepop : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 
 {
 
     public float sizeX;
     public float sizeY;
     public float sizeZ;
-ssss
     public void Start()
     {
         sizeX = gameObject.transform.localScale.x;
@@ -29,7 +28,7 @@ ssss
     {
         Debug.Log("The cursor entered the selectable UI element.");
 
-        gameObject.transform.localScale = new Vector3(sizeX * 2, sizeY * 2, sizeZ * 2);
+        gameObject.transform.localScale = new Vector3(sizeX * 1.25f, sizeY * 1.25f, sizeZ * 1.25f);
     }
 
 
