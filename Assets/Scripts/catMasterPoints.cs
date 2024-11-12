ca7ac4da20d94a37a11a7aaf2f4830887b410439
Bella99977 <Bella.lee@vis.tp.edu.tw>
2024-11-12 15:16:48 +0800

    0.04cat

---
+++ Assets/Scripts/catMasterPoints.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class catMasterPoints : MonoBehaviour
+{
+    // percieving
+    public int pPoints;
+    // judging
+    public int jPoints;
+    public int ePoints;
+    public int fPoints;
+    public int iPoints;
+    public int nPoints;
+    public int sPoints;
+    public int tPoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+    pPoints = 0 ;
+    judging = 0 ;
+     jPoints = 0 ;
+     ePoints = 0 ;
+     fPoints = 0 ;
+     iPoints = 0 ;
+     nPoints = 0 ;
+     sPoints = 0 ;
+     tPoints = 0 ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
